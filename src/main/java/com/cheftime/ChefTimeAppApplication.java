344d7410a3ac77d039ab1be111319a0a4ccd2b08
Daragh Farrell <daragh.farrell@gmail.com>
2017-01-18 13:08:02 +0000

    initial setup

---
+++ src/main/java/com/cheftime/ChefTimeAppApplication.java
@@ -0,0 +1,12 @@
+package com.cheftime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefTimeAppApplication.class, args);
+	}
+}
