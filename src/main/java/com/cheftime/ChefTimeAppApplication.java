3699a6375d3b643aefd11342d0dcb0b550b52e35
Daragh Farrell <daragh.farrell@gmail.com>
2017-01-18 13:08:02 +0000

    initial setup, fixed gradle version issue

--- src/main/java/com/cheftime/ChefTimeAppApplication.java
+++ src/main/java/com/cheftime/ChefTimeAppApplication.java
@@ -1,9 +1,19 @@
 package com.cheftime;
 
 import org.springframework.boot.SpringApplication;
+<<<<<<< HEAD
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+=======
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+>>>>>>> initial setup, fixed gradle version issue
 public class ChefTimeAppApplication {
 
 	public static void main(String[] args) {
