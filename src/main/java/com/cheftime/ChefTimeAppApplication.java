6fe1c6c16bfcfc2b0e599646fa5c0fa1730a622d
Daragh Farrell <daragh.farrell@gmail.com>
2017-01-19 12:17:40 +0000

    initial set up complete

--- src/main/java/com/cheftime/ChefTimeAppApplication.java
+++ src/main/java/com/cheftime/ChefTimeAppApplication.java
@@ -1,11 +1,6 @@
 package com.cheftime;
 
 import org.springframework.boot.SpringApplication;
-<<<<<<< HEAD
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-=======
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
->>>>>>> initial setup, fixed gradle version issue
 public class ChefTimeAppApplication {
 
 	public static void main(String[] args) {
